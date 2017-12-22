12de24b32f5fcbbb3b85516a08cf0b131af78230
qinyupeng <qinyupeng@qgutech.com>
2017-12-22 19:47:57 +0800

    登录开发-未完成

---
+++ src/main/java/com/qs/mvc/controller/login/LoginController.java
@@ -0,0 +1,7 @@
+package com.qs.mvc.controller.login;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+}
---
+++ src/main/java/com/qs/mvc/util/JsonStatus.java
@@ -0,0 +1,56 @@
+package com.qs.mvc.util;
+
+import java.io.Serializable;
+
+public class JsonStatus<T> implements Serializable {
+
+    public static final String CODE_OK = "200";
+    public static final String CODE_ERROR = "500";
+    public static final String CODE_LOSE = "404";
+
+    private String code;
+    private boolean success;
+    private String msg;
+    private T data;
+    private long count;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+}
